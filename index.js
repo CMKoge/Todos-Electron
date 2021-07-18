24d57119effccf7d0f9e6ca7a314d9693da72344
cmkoge <charunak1@gmail.com>
2021-07-18 23:33:35 +0530

    # Add functions to open another window

--- index.js
+++ index.js
@@ -3,21 +3,42 @@ const electron = require('electron')
 const { app, BrowserWindow, Menu } = electron
 
 let mainWindow
+let addWindow
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({})
     mainWindow.loadFile('main.html')
+    mainWindow.on('closed', () => app.quit()) // When main window close quit app with other sub window
     
     const mainMenu = Menu.buildFromTemplate(menuTemplate) // Build menu from template
     Menu.setApplicationMenu(mainMenu) // Put created menu into use
 })
+
+// Window for create new todos
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'New Todo',
+    })
+    addWindow.setMenu(null)
+    addWindow.loadFile('todo_create.html')
+}
 
 // Create menu template
 const menuTemplate = [
     {
         label: 'File',
         submenu: [
-            { label: 'New Todo' },
+            { 
+                label: 'New Todo',
+                // Click event
+                click() {
+                    createAddWindow()
+                },
+                // Add key hot key commibantion to click event
+                accelerator: process.platform == 'darwin' ? 'Commman+N' : 'Ctrl+N' 
+            },
             { 
                 label: 'Quit',
                 // Click event
