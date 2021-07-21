92a5e95dd9ff03f704017953380fc573a8e8de38
cmkoge <charunak1@gmail.com>
2021-07-21 23:26:18 +0530

    # Add functions to create new todo # Add functions clear tods

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const electron = require('electron')
 
-const { app, BrowserWindow, Menu } = electron
+const { app, BrowserWindow, Menu, ipcMain } = electron
 
 let mainWindow
 let addWindow
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({})
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true, 
+            contextIsolation: false
+        }
+    })
     mainWindow.loadFile('main.html')
     mainWindow.on('closed', () => app.quit()) // When main window close quit app with other sub window
     
@@ -20,10 +25,23 @@ function createAddWindow() {
         width: 300,
         height: 200,
         title: 'New Todo',
+        webPreferences: {
+            nodeIntegration: true, 
+            contextIsolation: false
+        }
     })
-    addWindow.setMenu(null)
+    // addWindow.setMenu(null)
     addWindow.loadFile('todo_create.html')
+    addWindow.on('closed', () => addWindow = null) // When add window close clear meomry of window(Collect grabage)
 }
+
+
+// Catch data from todo submit
+ipcMain.on('todoSubmit', (e, todo) => {
+    mainWindow.webContents.send('todoGet', todo) // Send data to main window3
+    addWindow.close()
+})
+
 
 // Create menu template
 const menuTemplate = [
@@ -31,13 +49,22 @@ const menuTemplate = [
         label: 'File',
         submenu: [
             { 
-                label: 'New Todo',
+                label: 'New',
                 // Click event
                 click() {
                     createAddWindow()
                 },
                 // Add key hot key commibantion to click event
                 accelerator: process.platform == 'darwin' ? 'Commman+N' : 'Ctrl+N' 
+            },
+            { 
+                label: 'Clear',
+                // Click event
+                click() {
+                  mainWindow.webContents.send('todoClear')
+                },
+                // Add key hot key commibantion to click event
+                accelerator: process.platform == 'darwin' ? 'Commman+D' : 'Ctrl+D' 
             },
             { 
                 label: 'Quit',
@@ -63,6 +90,7 @@ if (process.env.NODE_ENV != 'production') {
     menuTemplate.push({
         label: 'Developer',
         submenu: [
+            { role: 'reload' }, // Short cut to use conventional function
             {
                 label: 'Toggle Developer Tools',
                 click(item, focusedWindow) {
